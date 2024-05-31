d525becea43fa09deae15da2c15adafa19d339c5
Forest <767425412lin@gmail.com>
2024-05-31 18:12:55 +0800

    other: 调整响应体

--- internal/controller/hello/hello_v1_hello.go
+++ internal/controller/hello/hello_v1_hello.go
@@ -2,12 +2,21 @@ package hello
 
 import (
 	"context"
+	"net/http"
+
 	"github.com/gogf/gf/v2/frame/g"
 
-	"myapp/api/hello/v1"
+	v1 "myapp/api/hello/v1"
 )
 
 func (c *ControllerV1) Hello(ctx context.Context, req *v1.HelloReq) (res *v1.HelloRes, err error) {
-	g.RequestFromCtx(ctx).Response.Writeln("Hello World!")
+	name := g.Cfg("app.name")
+	g.RequestFromCtx(ctx).Response.WriteJson(g.Map{
+		"code":    http.StatusOK,
+		"message": "success",
+		"data": g.Map{
+			"name": name,
+		},
+	})
 	return
 }
